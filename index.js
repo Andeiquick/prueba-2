97e011d8698ae4ba730155ff3ca7b80dacd8816f
Andequick <145517852+Andeiquick@users.noreply.github.com>
2025-07-12 20:37:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.classList.toggle("active");
 }
+
+let currentSlide = 0;
+
+function moveTestimonial(step) {
+  const slides = document.querySelector('.testimonial-slider');
+  const totalSlides = slides.children.length;
+
+  currentSlide += step;
+
+  if (currentSlide >= totalSlides) currentSlide = 0;
+  if (currentSlide < 0) currentSlide = totalSlides - 1;
+
+  slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Auto slide cada 5 segundos
+setInterval(() => {
+  moveTestimonial(1);
+}, 5000);
